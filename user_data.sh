7df947cdb258b0979d5d67f3d35b365f6ffb6ced
vraparthi <vraparthi@proofpoint.com>
2020-06-27 23:07:55 -0700

    Added log for user data

--- user_data.sh
+++ user_data.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+exec > >(tee -i /var/log/user-data.log)
+exec 2>&1
+
 yum install -y unzip
 
 cd /tmp/ && curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip && sudo ./aws/install
